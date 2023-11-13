e9e5641d3292c86d120fcf25653168f68a458999
OmarMahmoud11 <akatskiamv65@gmail.com>
2023-11-13 19:44:52 +0200

    PET-1 The Structure of the project

---
+++ PVSSpringBoot/src/main/java/com/example/PVSSpringBoot/PvsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PVSSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvsSpringBootApplication.class, args);
+	}
+
+}
---
+++ PVSSpringBoot/src/test/java/com/example/PVSSpringBoot/PvsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PVSSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
